6c9fadf628a83b8a23b61fcf7f3fcbc57f6fa8e5
Sophiajoguinhos666 <132074657+Sophiajoguinhos666@users.noreply.github.com>
2024-08-16 12:20:46 -0300

    nb

--- script.js
+++ script.js
@@ -1,48 +1,46 @@
 const caixaPrincipal = document.querySelector('.caixa-principal');
-const caixaPergunta = document.querySelector('.caixa-pergunta');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
 const caixaResultado = document.querySelector('.caixa-resultado');
-const textoReultado = document.querySelector('.texto-reultado');
+const textoResultado = document.querySelector('.texto-resultado');
 
 const perguntas = [
     {
-        enunciado: "No ambito social, voce prefere:",
+        enunciado: "No âmbito social, você prefere:",
         alternativas: [
             {
-                texto: " Aperte o botão se você pudesse garantir a promoção de uma cultura de respeito e empatia em todos os ambientes de trabalho, mas isso significaria que você teria que abrir mão de algumas liberdades pessoais, como discutir opiniões controversas publicamente.",
-                afirmação: "afirmação",
+                texto: "Aperte o botão se você pudesse garantir a promoção de uma cultura de respeito e empatia em todos os ambientes de trabalho, mas isso significaria que você teria que abrir mão de algumas liberdades pessoais, como discutir opiniões controversas publicamente.",
+                afirmacao: "afirmação",
             },
             {
-                texto: "Aperte o botão se você pudesse ter um aumento significativo na sua inteligência e criatividade, mas isso significaria não conseguir mais se conectar emocionalmente com as pessoas da mesma forma que antes.",
-                afirmação: "afirmação",
+                texto: "alternativa 2",
+                afirmacao: "afirmação",
             },
         ]
     },
-
     {
-        enunciado: "No ambito ambiental, voce prefere:",
+        enunciado: "No âmbito ambiental, você prefere:",
         alternativas: [
             {
-                texto: "  Aperte o botão se você pudesse assegurar a limpeza dos oceanos de todos os resíduos plásticos, mas isso implicaria na proibição de todos os plásticos novos por dez anos.",
-                afirmação: "afirmação",
+                texto: "alternativa 3",
+                afirmacao: "afirmação",
             },
             {
-                texto: " Aperte o botão se você pudesse garantir que todas as fontes de energia no planeta se tornassem 100% renováveis, mas isso exigiria que você não pudesse usar veículos particulares e precisasse depender exclusivamente de transporte público e alternativas sustentáveis.",
-                afirmação: "afirmação",
+                texto: "alternativa 4",
+                afirmacao: "afirmação",
             },
         ]
     },
-
     {
-        enunciado: "No ambito tecnologico, voce prefere:",
+        enunciado: "No âmbito tecnológico, você prefere:",
         alternativas: [
             {
-                texto: "  Aperte o botão se você pudesse ter acesso ilimitado a qualquer avanço tecnológico e inovações futuras, mas isso significaria que você teria que abrir mão de todos os dispositivos tecnológicos atuais por cinco anos.",
-                afirmação: "afirmação",
+                texto: "alternativa 5",
+                afirmacao: "afirmação",
             },
             {
-                texto: " Aperte o botão se você pudesse ter um dispositivo que automaticamente utiliza se a sua saúde e bem-estar com base em dados biométricos em tempo real, mas isso significaria que você teria que abrir mão de todos os aplicativos de redes sociais e de entretenimento.",
-                afirmação: "afirmação",
+                texto: "alternativa 6",
+                afirmacao: "afirmação",
             },
         ]
     },
@@ -52,21 +50,18 @@ let atual = 0;
 let perguntaAtual;
 
 function mostraPergunta (){
-      perguntaAtual = perguntas[atual];
-      caixaPergunta.textContent = perguntaAtual.enunciado;
-      caixaAlternativas.textContent = "";
-      mostraAlternativa();
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent ="";
+    mostraAlternativa();
 }
 
 function mostraAlternativa (){
     for (const pergunta of perguntaAtual.alternativas) {
-        const botaoAlternativa = document.createElement('button')
-        botaoAlternativa.textContent = pergunta.texto;
-        botaoAlternativa.addEventListener("click", function(){
-            atual++;
-            mostraPergunta();
-        })
-        caixaAlternativas.appendChild(botaoAlternativa);
+       const botaoAlternativa = document.createElement('button');
+       botaoAlternativa.textContent = pergunta;
+       caixaAlternativas.appendChild(botaoAlternativa);
     }
 }
+
 mostraPergunta();
